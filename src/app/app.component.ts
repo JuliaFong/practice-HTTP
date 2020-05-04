1a4b2f512aac7f80dcb3e1d74dc2e3a5d0a005ec
Julia Fong <Juliamzfong@gmail.com>
2020-05-03 23:57:36 -0500

    Add error handles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 import { Post } from './post.model';
 import { PostsService } from './posts.service';
@@ -10,13 +11,19 @@ import { PostsService } from './posts.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadedPosts: Post[] = [];
   isFetching = false;
+  error = null;
+  private errorSub: Subscription
 
   constructor(private http: HttpClient, private postsService: PostsService) {}
 
   ngOnInit() {
+   this.errorSub = this.postsService.error.subscribe(errorMessage => {
+      this.error = errorMessage;
+    })
+
     this.isFetching = true;
     this.postsService.fetchPosts()
     .subscribe(posts => {
@@ -37,6 +44,9 @@ export class AppComponent implements OnInit {
     .subscribe(posts => {
       this.isFetching = false;
       this.loadedPosts = posts
+    }, error => {
+      this.error = error.message;
+      console.log(error)
     })
   }
 
@@ -46,6 +56,10 @@ export class AppComponent implements OnInit {
     .subscribe(() => {
       this.loadedPosts = [];
     })
+  }
+
+  ngOnDestroy() {
+    this.errorSub.unsubscribe() 
   }
 
 }
